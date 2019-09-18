e4b36ebc226f31d81b824501fe23658021fc2ccb
Marti FIGUERAS MARTINEZ <mfiguera@e1r6p21.42.fr>
2019-09-18 11:05:35 +0200

    adapted Variable to new Operands

--- solving/Variable.py
+++ solving/Variable.py
@@ -6,7 +6,7 @@
 #    By: mfiguera <mfiguera@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/09/17 09:37:12 by mfiguera          #+#    #+#              #
-#    Updated: 2019/09/17 19:36:30 by mfiguera         ###   ########.fr        #
+#    Updated: 2019/09/18 11:04:35 by mfiguera         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -18,26 +18,23 @@ class Variable():
 
     def __init__(self):
         #could add permanent value here and update when check_value is run
-        pass
-        self.true = None
-        self.false = 0
+        self.val = False
+        self.certain = False
 
 
-    def set_value(self, newval):
-        if newval == True:
-            self.true = 1
-        elif newval == False:
-            self.false = 1
-        return self.check_value()
-
-
-    def check_value(self):
-        if self.true == self.false:
+    def set_val(self, newval):
+        if not self.certain:
+            self.value = newval
+            self.certain = True
+        elif newval != self.value:
             sys.exit('ERROR - Non solvable system. {}'.format(self.get_error_unsolvable()))
-        elif self.true == 1:
-            return True
-        else:
-            return False
+
+
+    def read_val(self):
+        if self.certain:
+            return self.value
+        return None
+
     
     def get_error_unsolvable(self):
         pass
