9816ef2c02e7b96e4b54ad205bd1c4e63bb6791c
Marti FIGUERAS MARTINEZ <mfiguera@e1r6p21.42.fr>
2019-09-18 11:05:04 +0200

    definition of operands

--- solving/Operands.py
+++ solving/Operands.py
@@ -6,7 +6,7 @@
 #    By: mfiguera <mfiguera@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2019/09/17 18:58:52 by mfiguera          #+#    #+#              #
-#    Updated: 2019/09/17 19:24:43 by mfiguera         ###   ########.fr        #
+#    Updated: 2019/09/18 11:04:28 by mfiguera         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -17,32 +17,75 @@ sys.path.insert(1, './solving/')
 from Variable import Variable
 
 
-class Operand(Variable):
-    pass
-
-
-class Logicand(Variable):
-    pass
-
-
 class Logicnot(Variable):
+    """
+    Logic gate NOT
+    Allows one input and one output. Output is True if input is False and viceversa
+    """
 
     def __init__(self, term):
+        super().__init__()
         self.term = term
-        self.value = None
 
-    def check_value(self):
-        return not self.term.check_values()
+
+    def read_val(self):
+        if self.term.certain:
+            return not self.term.read_val()
+
 
     # def answer_mode(self, ans):
     #     ans = not ans
-    #     self.term.set_value(ans)
+    #     self.term.set_val(ans)
+
+
+
+class Logicand(Variable):
+    """
+    Logic gate AND
+    Allows 2+ inputs and one output. Output is True only if all inputs are True
+    """
+
+    def __init__(self, terms):
+        super().__init__()
+        self.terms = terms
+
+
+    def read_val(self):
+        if all(term.certain == True for term in self.terms):
+            return all(term.val == True for term in self.terms)
+        elif any(term.certain == True and term.val == False for term in self.terms):
+            return False
+
+
+    # def answer_mode(self, ans):
+    #     ans = not ans
+    #     self.term.set_val(ans)
 
 
 
 class Logicor(Variable):
-    pass
+    """
+    Logic gate AND
+    Allows 2+ inputs and one output. Output is True if at least one of the inputs is True
+    """
+    
+    def __init__(self, terms):
+        super().__init__()
+        self.terms = terms
+
+
+    def read_val(self):
+        if all(term.certain == True for term in self.terms):
+            return any(term.val == True for term in self.terms)
+        elif any(term.certain == True and term.val == True for term in self.terms):
+            return True
+
 
 
 class Logicxor(Variable):
+    """
+    Logic gate XOR
+    Allows 2 inputs and one output. Output is True if inputs are not equal
+    """
+    
     pass
